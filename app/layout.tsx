7d146ff07b50dc259f8c8221c089a87482222471
hori19991021 <hori19991021@gmail.com>
2026-04-05 17:49:35 +0900

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Kanji Quiz",
+  description: "Kanji Quiz App",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+}
